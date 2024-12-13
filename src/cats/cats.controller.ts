bf02d12c7d1705cace3d4b7bf1746cbb845f588a
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2024-12-13 19:22:52 +0700

    update

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,16 +1,32 @@
-import { Controller , Get , Post , HttpCode, Req} from '@nestjs/common';
-import { Request } from 'express';
 
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateCatDto} from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+import { ListAllEntities} from './dto/list-all-entities';
 @Controller('cats')
 export class CatsController {
   @Post()
-  @HttpCode(201)
-  create() {
+  create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
   @Get()
-  findAll(@Req() request: Request): string {
-    return 'This action returns all cats';
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all cats (limit: ${query.limit} items)`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
---
+++ src/cats/dto/update-cat.dto.ts
@@ -0,0 +1,10 @@
+
+
+
+
+export class UpdateCatDto{
+    name: string;
+    age: number;
+    breed: string;
+  }
+  
