efb2e701489dd129d21ca85582fc87a09d5f83a2
Alfan <138642451+Alfan14@users.noreply.github.com>
2024-12-14 17:21:29 +0700

    Update connection.provider.ts

--- src/database/connection.provider.ts
+++ src/database/connection.provider.ts
@@ -1,17 +1,1 @@
 
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from '../users/users.module';
-
-export const Connection = TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'simform',
-  database: 'pgWithNest',
-  entities: [],
-  synchronize: true,
-  logging: true,
-});
-
-// These is a word
