8f3f711e6302ccc65f4de2bdd2b6c49125cb445b
root <root@LAPTOP-CE7UFLHI.>
2024-12-21 12:05:52 +0700

    add constants

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,30 +10,33 @@ export class AuthService {
     saltOrRounds: number = 10;
 
     constructor(
-      private usersService: UsersService ,
+      private usersService: UsersService,
       private jwtService: JwtService
     ) {}
   
-  async signIn(email, pass) {
-    async signIn(email, password) {
-        const user = await this.usersService.findOne(email);
-        if (user?.password !== pass) {
-        const isMatch = await bcrypt.compare(password, user?.password);
+   
+    async signIn(email: string, password: string) {
+      const user = await this.usersService.findOne(email);
     
-        if (!isMatch) {
-          throw new UnauthorizedException();
-        }
-    
-        const payload = { sub: user.userId, email: user.email };
-    
-        return {
-          access_token: await this.jwtService.signAsync(payload),
-        };
+      if (!user) {
+        throw new UnauthorizedException('Invalid credentials');
       }
+    
+      const isMatch = await bcrypt.compare(password, user.password);
+    
+      if (!isMatch) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+    
+      const payload = { sub: user.userId, email: user.email };
+    
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
     }
-  }
+    
+  
     async signUp(payload: CreateUserDto) {
-      const user = await this.usersService.create(payload);
       const hashPass = await bcrypt.hash(payload.password, this.saltOrRounds)
   
       let data = {
